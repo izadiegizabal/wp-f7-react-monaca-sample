6b77f788197494d8f18b00acace3f39400d1d3d6
Izadi <64izadi@gmail.com>
2020-06-17 11:56:59 +0900

    feat: add infinite scroll

--- app/src/pages/posts.jsx
+++ app/src/pages/posts.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import renderHTML from 'react-render-html';
 import { Page, Navbar, List, ListItem } from 'framework7-react';
+import './css/posts.css';
 
 const API =
   'https://public-api.wordpress.com/wp/v2/sites/monacasampleapp.wordpress.com/';
 const ALL_POSTS = 'posts?';
 const PER_PAGE = 'per_page=';
 const PAGE = 'page=';
+let PAGE_NUM = '1';
 
 export default class extends React.Component {
   constructor() {
@@ -14,6 +16,8 @@ export default class extends React.Component {
 
     this.state = {
       posts: [],
+      allowInfinite: true,
+      showPreloader: true,
     };
   }
 
@@ -21,9 +25,15 @@ export default class extends React.Component {
     const { posts } = this.state;
 
     return (
-      <Page name="posts">
+      <Page
+        name="posts"
+        infinite
+        infiniteDistance={50}
+        infinitePreloader={this.state.showPreloader}
+        onInfinite={this.loadMore.bind(this)}
+      >
         <Navbar title="WP App" />
-        <List>
+        <List className="main-list">
           {posts.map((post) => (
             <ListItem
               key={post.id}
@@ -38,11 +48,35 @@ export default class extends React.Component {
 
   componentDidMount() {
     // Fetch data
-    fetch(API + ALL_POSTS + PER_PAGE + '20' + '&' + PAGE + '1')
+    fetch(API + ALL_POSTS + PER_PAGE + '20' + '&' + PAGE + PAGE_NUM)
       .then((response) => response.json())
       .then((data) => {
         this.setState({ posts: data });
-        console.log(this.state);
+      });
+  }
+  loadMore() {
+    const self = this;
+    if (!self.state.allowInfinite) return;
+    self.setState({ allowInfinite: false });
+
+    PAGE_NUM++;
+    fetch(API + ALL_POSTS + PER_PAGE + '20' + '&' + PAGE + PAGE_NUM)
+      .then((response) => response.json())
+      .then((data) => {
+        const items = self.state.posts;
+        if (items.length >= 1000) {
+          self.setState({ showPreloader: false });
+          return;
+        }
+
+        data.forEach((post) => {
+          items.push(post);
+        });
+
+        self.setState({
+          items,
+          allowInfinite: true,
+        });
       });
   }
 }
